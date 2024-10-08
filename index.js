e3c5994144ef5e753b05db818e2f3ac2321a3d85
tt1nK <dekogo181@gmail.com>
2024-10-08 16:50:30 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log(333);
+
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Hello мир ;)'
+});
